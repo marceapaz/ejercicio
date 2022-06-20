98bb1a8084fec878ad924cd1d8e4edc04008e9ab
marceloapaz <marceysoleapazvidela@gmail.com>
2022-06-20 10:58:54 -0300

    ejercicio numero 3

---
+++ ejerciciopractica3.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+def aDecimal(numeroBin):
+    numeroBin = str(numeroBin)
+    decimal = 0
+    exp = len (numeroBin) -1
+    for i in numeroBin:
+        decimal += (int(i) * 2**(exp))
+        exp = exp - 1
+    return decimal
